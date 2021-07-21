9b8152f44aabc4a7d7b9af0de7a22bbe75376a19
jhin93 <scene1993@gmail.com>
2021-07-21 16:14:32 +0900

    Web3 & 컨트랙 인스턴스화 완료

--- Blockchain/real-estate/5.2 real-estate-starter/src/js/app.js
+++ Blockchain/real-estate/5.2 real-estate-starter/src/js/app.js
@@ -17,14 +17,27 @@ App = {
         list.append(template.html());
       }
     })
+
+    return App.initWeb3();
   },
 
   initWeb3: function() {
-	
+    if(typeof web3 !== 'undefined'){
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+  
+    return App.initContract();
   },
 
   initContract: function() {
-		
+		$.getJSON('RealEstate.json', function(data){
+      App.contracts.RealEstate = TruffleContract(data);
+      App.contracts.RealEstate.setProvider(App.web3Provider);
+    })
   },
 
   buyRealEstate: function() {	
