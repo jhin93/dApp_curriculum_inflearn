b217e5fd3563dc859d788459e2adb28ffa503eb2
jhin93 <scene1993@gmail.com>
2021-07-26 00:25:15 +0900

    Ropsten 테스트넷 컨트랙 배포 및 테스팅 II (인퓨라 사용) 완료

--- Blockchain/real-estate/5.2 real-estate-starter/truffle.js
+++ Blockchain/real-estate/5.2 real-estate-starter/truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "umbrella hospital alley rent elephant tilt link river inch doll joke above";
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -6,6 +9,14 @@ module.exports = {
                host: "localhost",
                port: 8545,
                network_id: "*" // Match any network id
+          },
+          ropsten: {
+               provider: function() {
+                    return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/27e974853e00447fa5db3b3a1c9d5045')
+               },
+               network_id: '3',
+               gas: 4500000,
+               gasPrice: 10000000000,
           }
      }
 };
